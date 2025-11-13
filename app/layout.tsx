c161a594789053b08663d2679a3136520ed5e1ad
Abhishek Kumar <abhishek.kumar@jvfpl.co>
2025-11-13 20:51:47 +0530

    add vercal analystics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
-
+import {Analytics} from '@vercel/analytics/react';
 
 // import { pageRegistry } from "../config/pageRegistry";
 
@@ -39,6 +39,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer/>
+        <Analytics />
       </body>
     </html>
   );
